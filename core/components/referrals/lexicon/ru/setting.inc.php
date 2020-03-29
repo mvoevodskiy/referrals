7bf90a588189ef70db64f94c580d7b6ed97b7e88
mvoevodskiy <mv@aaa0.ru>
2020-03-30 02:45:40 +0300

    Added lexicon for deprecated system setting

--- core/components/referrals/lexicon/ru/setting.inc.php
+++ core/components/referrals/lexicon/ru/setting.inc.php
@@ -4,4 +4,4 @@ $_lang['area_referrals_main'] = 'Основные';
 
 $_lang['setting_referrals_some_setting'] = 'Какая-то настройка';
 $_lang['setting_referrals_some_setting_desc'] = 'Это описание для какой-то настройки';
-$_lang['setting_referrals_max_percent_for_use'] = '[УСТАРЕЛА] Используйте <b>referrals_use_limit</b>';
+$_lang['setting_referrals_max_percent_for_use'] = '[УСТАРЕЛА] Используйте _referrals_use_limit_';
