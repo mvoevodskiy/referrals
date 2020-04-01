594417ced8ecbe04df37b116aa5267c5eb8dd29e
mvoevodskiy <mv@aaa0.ru>
2020-04-01 10:29:19 +0300

    Added field createdon to refUser model

--- core/components/referrals/model/referrals/refuser.class.php
+++ core/components/referrals/model/referrals/refuser.class.php
@@ -5,6 +5,9 @@ class refUser extends xPDOSimpleObject {
     public function save($cacheFlag= null)
     {
         $new = $this->isNew();
+        if ($new && empty($this->createdon)) {
+            $this->set('createdon', time());
+        }
         if (parent::save($cacheFlag)) {
             if ($new) {
                 refLog::write($this->xpdo, refLog::ACTION_REGISTER, $this->get('user'));
