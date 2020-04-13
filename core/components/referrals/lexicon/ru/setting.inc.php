b6b5e1e19c78586662715f4b5e6dea403758735d
mvoevodskiy <mv@aaa0.ru>
2020-04-13 15:53:31 +0300

    Added lexicon entries for system settings

--- core/components/referrals/lexicon/ru/setting.inc.php
+++ core/components/referrals/lexicon/ru/setting.inc.php
@@ -1,7 +1,35 @@
 <?php
 
 $_lang['area_referrals_main'] = 'Основные';
+$_lang['area_referrals_accounts'] = 'Типы счетов/счета';
+$_lang['area_referrals_values'] = 'Значения';
+$_lang['area_referrals_statuses'] = 'Статусы заказов';
+$_lang['area_referrals_fields'] = 'Поля';
+$_lang['area_referrals_ttl'] = 'Срок жизни';
 
 $_lang['setting_referrals_some_setting'] = 'Какая-то настройка';
 $_lang['setting_referrals_some_setting_desc'] = 'Это описание для какой-то настройки';
+
 $_lang['setting_referrals_max_percent_for_use'] = '[УСТАРЕЛА] Используйте _referrals_use_limit_';
+$_lang['setting_referrals_confirm_field'] = '[УСТАРЕЛА]';
+$_lang['setting_referrals_account_type_id_referrals'] = 'ID счета для учета приглашенных';
+$_lang['setting_referrals_account_type_id_money'] = 'ID бонусного (денежного) счета';
+$_lang['setting_referrals_use_limit'] = 'Лимит использования бонусов в заказе. Допустим %';
+$_lang['setting_referrals_level_rewards'] = 'Вознаграждения по уровням';
+$_lang['setting_referrals_reward_register_master'] = 'Вознаграждение за регистрацию тому, кто привел';
+$_lang['setting_referrals_reward_register_user'] = 'Вознаграждение за регистрацию приведенному';
+$_lang['setting_referrals_order_status_decrease'] = 'ID статуса заказа для списания бонусов. Обычно 1 - Новый';
+$_lang['setting_referrals_order_status_increase'] = 'ID статуса заказа для возврата бонусов. Обычно 4 - Отменен';
+$_lang['setting_referrals_order_status_reward'] = 'ID статуса заказа для начисления вознаграждений. Обычно 2 - Оплачен';
+$_lang['setting_referrals_order_status_revoke'] = 'ID статуса заказа для отмены начисления. Обычно 4 - Отменен';
+$_lang['setting_referrals_level_rewards_product_field'] = 'Поле товара для вознаграждений по уровням';
+$_lang['setting_referrals_use_limit_product_field'] = 'Поле товара для лимита использования бонусов';
+$_lang['setting_referrals_var_url'] = 'Имя переменной в URL';
+$_lang['setting_referrals_var_cookie'] = 'Имя cookie';
+$_lang['setting_referrals_action_script'] = 'URI скрипта обработки AJAX-запросов ';
+$_lang['setting_referrals_frontend_js'] = 'URI файла JavaScript';
+$_lang['setting_referrals_sms_login'] = 'Логин от сервиса sms.ru';
+$_lang['setting_referrals_sms_password'] = 'Пароль от сервиса sms.ru';
+$_lang['setting_referrals_cookie_ttl'] = 'Время жизни cookie с ID пригласившего (дни)';
+$_lang['setting_referrals_reward_ttl'] = 'Время жизни вознаграждений (дни)';
+$_lang['setting_referrals_referral_ttl'] = 'Время действия привязки к пригласившему (дни)';
